f8533a6932da53b716e7156a388a92f07faffaaa
Lowell Bander <lowellbander@gmail.com>
2014-08-30 08:07:05 -0700

    does simple family lookup

--- run.py
+++ run.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import unirest
 
 app = Flask(__name__)
 app.debug = True
 
+def get(famID):
+    response = unirest.get(os.environ['CLERK_URL'] + 'tour/' + famID)
+    return response.body
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    famID = request.args.get('id')
+    if famID is None:
+        return render_template('index.html')
+    else:
+        fam = get(famID)
+        try:
+            name = fam['name']
+        except TypeError:
+            return "invalid ID in URL"
+        else:
+            return "rendering form for %s ..." % name
 
 if __name__ == '__main__':
     app.run()
